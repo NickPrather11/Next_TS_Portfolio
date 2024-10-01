bc19931565edc26634343f624425677c36dee7ff
unknown <nickprather11@gmail.com>
2024-09-30 22:15:41 -0400

    fix: editing styling on /about/site page, testing for mobile screen

--- app/about/site/page.tsx
+++ app/about/site/page.tsx
@@ -14,30 +14,28 @@ interface ILogo {
 const aboutSite = () => {
   const item: Array<ILogo> = dbSeed;
   return (
-    <Center className="flex-col px-6 m-4">
+    <Center className="flex-col">
       <Card className="md:w-8/12">
         <h2 className="text-center">About This Site</h2>
 
-        <div id="about_site">
-          <p>
-            Thanks for checking out my portfolio! This site is a responsive
-            React web application built with the Next.js framework in a Node
-            environment using Typescript as the base language. Styling has been
-            added using TailwindCSS. This site contains data retrieved from a
-            MongoDB database using Mongoose.
-            <br></br>
-            <br></br>
-            If you&apos;d like to check out the code repo for this site and see
-            the updates I have planned, see the links below.
-          </p>
-        </div>
+        <p>
+          Thanks for checking out my portfolio! This site is a responsive React
+          web application built with the Next.js framework in a Node environment
+          using Typescript as the base language. Styling has been added using
+          TailwindCSS. This site contains data retrieved from a MongoDB database
+          using Mongoose.
+          <br></br>
+          <br></br>
+          If you&apos;d like to check out the code repo for this site and see
+          the updates I have planned, see the links below.
+        </p>
 
         <div className="flex flex-col gap-8 mt-8">
           <div className="flex flex-row flex-wrap gap-2">
             <p>Site Repo:</p>
             <Link
               href="https://github.com/NickPrather11/Next_TS_Portfolio"
-              className="underline text-blue-500 hover:text-blue-700 visited:text-purple-600 w-full text-wrap break-words"
+              className="underline text-blue-500 hover:text-blue-700 visited:text-purple-600 w-full text-sm break-words"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -48,7 +46,7 @@ const aboutSite = () => {
             <p>Planned Updates:</p>
             <Link
               href="https://github.com/users/NickPrather11/projects/1"
-              className="underline text-blue-500 hover:text-blue-700 visited:text-purple-600 w-full text-wrap break-words"
+              className="underline text-blue-500 hover:text-blue-700 visited:text-purple-600 w-full text-sm break-words"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -63,7 +61,7 @@ const aboutSite = () => {
           <p className="italic text-sm">
             This site was built using the following technologies
           </p>
-          <ul className="flex flex-wrap justify-evenly items-center gap-16">
+          <ul className="flex flex-wrap justify-evenly items-center gap-12">
             {item.map((logo: ILogo) => (
               <div key={logo.name}>
                 <TechCard
