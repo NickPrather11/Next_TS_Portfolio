9f2b537090ca4122a3c5f2032d9240e4c61cf798
Nick Prather <nickprather11@gmail.com>
2024-10-13 13:42:22 -0400

    update: Lebowski Trivia project description updated

--- app/db/seeds/dbProjectsSeed.ts
+++ app/db/seeds/dbProjectsSeed.ts
@@ -17,7 +17,7 @@ const dbProjectsSeed: IProject[] = [
     title: "Lebowski Trivia",
     descriptionShort: "Trivia game based on the movie 'The Big Lebowski'",
     descriptionExtended:
-      "This is a trivia game involving the film 'The Big Lebowski'. You have 120 seconds to answer all 15 questions. When you are finished answering, just hit the 'submit' button. If you run out of time, any unanswered questions will be counted as incorrect. When you are finished, you'll be directed to a page displaying your score as 'n/15'. Any questions answered incorrectly will be shown with your answer (marked as incorrect) and the correct answer. Click the 'Try Again' button to take the quiz again!",
+      "This is a trivia game about the film 'The Big Lebowski'. You have 120 seconds to answer all 15 questions. When you are finished answering, click the 'submit' button. If you run out of time, any unanswered questions will be counted as incorrect. When you are finished, you will be directed to a page displaying your score as 'x/15'. Any questions answered incorrectly will be shown with your answer (marked as incorrect) and the correct answer. Click the 'Try Again' button to take the quiz again!",
   },
   {
     slug: "gluttony_game",
