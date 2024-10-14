86d1528230fad5950922b7d165fa527fb1464dde
Nick Prather <nickprather11@gmail.com>
2024-10-14 15:23:00 -0400

    update: Gluttony game descriptions updated

--- app/db/seeds/dbProjectsSeed.ts
+++ app/db/seeds/dbProjectsSeed.ts
@@ -27,7 +27,7 @@ const dbProjectsSeed: IProject[] = [
     title: "Gluttony! Game",
     descriptionShort: "Memory game with a hedonistic theme. Indulge yourself!",
     descriptionExtended:
-      "You will be given a random number at the start of the game. This represents your ideal, transcendental buzz acquired from gobbling down all the items. There are five items below that will help you reach your goal, each item worth a random value. By clicking on the items, your score will increase by this random value. You must reach your goal number to win, going above this number causes you to get sick and lose. Each time the game starts, you goal number and the random values change!",
+      "You will be given a random number at the start of the game. This represents your ideal, transcendental buzz acquired from gobbling down all the items.There are five items displayed that will help you reach your goal, each item worth a random number value. By clicking on the items, your score will increase by this random value. You must reach your goal number to win; going above this number causes you to get sick and lose. Each time the game starts, your goal number and the random values change!",
   },
 ];
 
--- app/projects/gluttony_game/page.tsx
+++ app/projects/gluttony_game/page.tsx
@@ -11,9 +11,9 @@ const gluttonyGamePage = () => {
     hrefDeployed: "https://nickprather11.github.io/gluttony_game/",
     hrefRepo: "https://github.com/NickPrather11/gluttony_game",
     title: "Gluttony! Game",
-    descriptionShort: "Memory game with a hedonistic theme...",
+    descriptionShort: "Memory game with a hedonistic theme. Indulge yourself!",
     descriptionExtended:
-      "You will be given a random number at the start of the game. This represents your ideal, transcendental buzz acquired from gobbling down all the items. There are five items below that will help you reach your goal, each item worth a random value. By clicking on the items, your score will increase by this random value. You must reach your goal number to win, going above this number causes you to get sick and lose. Each time the game starts, you goal number and the random values change!",
+      "You will be given a random number at the start of the game. This represents your ideal, transcendental buzz acquired from gobbling down all the items.There are five items displayed that will help you reach your goal, each item worth a random number value. By clicking on the items, your score will increase by this random value. You must reach your goal number to win; going above this number causes you to get sick and lose. Each time the game starts, your goal number and the random values change!",
   };
 
   return (
